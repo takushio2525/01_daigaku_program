337e77eda2a1369a6afc82e3e1c6440ed95af4fb
takushioMac <shiozawataku2525@gmail.com>
2025-06-03 09:23:43 +0900

    データベースの授業した

---
+++ 01_jouhougaironn/6_2/Database/student23Table.sql
@@ -0,0 +1,12 @@
+-- SQLite
+-- SQLite
+CREATE TABLE student23 (
+  id  char(7) PRIMARY KEY NOT NULL,
+  Family_name varchar(50) NOT NULL,
+  First_name varchar(50) NOT NULL,
+  Japanese_score integer NOT NULL,
+  English_score integer NOT NULL,
+  Math_score integer NOT NULL,
+  Club varchar(50) ,
+  LOA bool NOT NULL
+);
