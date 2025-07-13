9e0d2de8ed37c2915fb2be7262c8481495d703ec
takushioMac <shiozawataku2525@gmail.com>
2025-07-14 00:37:16 +0900

    うわああああああ

--- 00_program_genngo/11_repo/patternlib.c
+++ 00_program_genngo/11_repo/patternlib.c
@@ -1,29 +1,103 @@
 #include <stdio.h>
+#include "patternlib.h"
 
-void printx(int flag)
+void print_char(int flag, char _char)
 {
     if (flag == 0)
+    {
         printf(" ");
+    }
     else
-        printf("X");
-}
-
-void print_char_scaled(char c, int scale)
-{
-    for (int i = 0; i < scale; i++)
     {
-        printf("%c", c);
+        if (_char == ' ')
+        {
+            printf("\e[47m%c\e[0m", _char);
+        }
+        else
+        {
+            printf("%c", _char);
+        }
     }
 }
 
-void printx_scaled(int flag, int scale)
+void print_char_colored(int flag, char _char, int color)
 {
     if (flag == 0)
+        printf("\e[0m ");
+    else
     {
-        print_char_scaled(' ', scale);
+        if (_char == ' ')
+        {
+            printf("\e[4%dm%c\e[0m", color, _char);
+        }
+        else
+        {
+            printf("\e[3%dm%c\e[0m", color, _char);
+        }
+    }
+}
+
+void show_array_vertical_scaled(int array[][ROW_SIZE][COL_SIZE], char _char, int len_c, int scale, int color_flag)
+{
+
+    for (int c = 0; c < len_c; c++)
+    {
+
+        for (int y = 0; y < ROW_SIZE * scale; y++)
+        {
+            for (int x = 0; x < COL_SIZE; x++)
+            {
+                for (int s = 0; s < scale; s++)
+                {
+                    if (color_flag)
+                    {
+                        print_char_colored(array[c][y / scale][x], _char, array[c][y / scale][x]);
+                    }
+                    else
+                    {
+                        print_char(array[c][y / scale][x], _char);
+                    }
+                }
+            }
+            printf("\n");
+        }
+    }
+}
+
+void show_array_horizontal_scaled(int array[][ROW_SIZE][COL_SIZE], char _char, int len_c, int scale, int color_flag)
+{
+
+    for (int y = 0; y < ROW_SIZE * scale; y++)
+    {
+        for (int c = 0; c < len_c; c++)
+        {
+            for (int x = 0; x < COL_SIZE; x++)
+            {
+                for (int s = 0; s < scale; s++)
+                {
+                    if (color_flag)
+                    {
+                        print_char_colored(array[c][y / scale][x], _char, array[c][y / scale][x]);
+                    }
+                    else
+                    {
+                        print_char(array[c][y / scale][x], _char);
+                    }
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+void show_array(int array[][ROW_SIZE][COL_SIZE], char _char, int len_c, int scale, int color_flag, int horizontal_flag)
+{
+    if (horizontal_flag)
+    {
+        show_array_horizontal_scaled(array, _char, len_c, scale, color_flag);
     }
     else
     {
-        print_char_scaled('X', scale);
+        show_array_vertical_scaled(array, _char, len_c, scale, color_flag);
     }
 }
